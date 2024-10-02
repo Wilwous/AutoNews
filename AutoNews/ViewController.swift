392cd3271f7b121fffbb616bb684760abe58b91e
Wilwous <mick.thomson.tm@gmail.com>
2024-10-02 13:46:10 +0300

    Initial Commit

---
+++ AutoNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoNews
+//
+//  Created by Антон Павлов on 02.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
